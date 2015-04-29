cd2d134a3b896bfbe5c67ff81fd31c55bbb0b353
Catherine Lau <clau.catherine@gmail.com>
2015-04-29 16:29:48 -0400

    Basic twitter clone working.  Pretty.

--- app.js
+++ app.js
@@ -1,14 +1,31 @@
 var express = require( 'express' );
 var morgan = require('morgan');
+var swig = require('swig');
+var routes = require('./routes/');
+
 
 // initialize app
 var app = express();
 
+app.engine('html', swig.renderFile);
+swig.setDefaults({ cache: false });
+
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views/');
 app.use(morgan('dev'));
-
-app.get('/', function(request, response) {
-	console.log("server listening");
-	response.send("working");
-});
-
+app.use(express.static(__dirname + '/public'));
 app.listen(3000);
+app.use('/', routes);
+
+
+// Data
+var pplArr = [{name: 'John'}, {name: 'Jane'}, {name: 'Joe'}];
+var siteData = {title: 'Hall of Fame', people: pplArr};
+
+// app functionality
+
+/*app.get('/', function(request, response) {
+	console.log("server listening");
+	response.render('index', siteData);
+});*/
+
